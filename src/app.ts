d8e5c7ed47bd6e737feab3352342180f4a782737
PedroStupp <75598@PROWAY.TREINA>
2025-02-20 22:01:57 -0300

    ...

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express'
+import cors from "cors"
+import bodyParser from 'body-parser'
+import userRoutes from './routes/user.routes'
+
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use("/user",userRoutes)
 
 export default app
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+export const UserController = {
+    getAll : (req: Request, res: Response) => {
+        res.send("trazer usus")
+    },
+    getById : (req: Request, res: Response) => {
+
+    },
+    create: (req: Request, res: Response) => {
+        
+    },
+    update: (req: Request, res: Response) => {
+
+    },
+    delete: (req: Request, res: Response) => {
+        
+    }
+}
