aec121a4eef3b70475c63fa0f0fdda90969ce43a
Pedro Henrique de Araujo Stupp <75598@PROWAY.TREINA>
2025-02-21 22:00:08 -0300

    aaaa

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from "express";
+import { userService } from "../services/user.service";
 
 export const UserController = {
     getAll : (req: Request, res: Response) => {
-        res.send("trazer usus")
+        res.json(userService.findAll())
     },
     getById : (req: Request, res: Response) => {
-
+        const user = userService.findById(Number(req.params.id))
+        user ? res.json(user) : res.status(404).json({message: "Usuario nao encontrado :3"})
     },
     create: (req: Request, res: Response) => {
-        
+        const {name, email} = req.body
+        const user = userService.create(name, email)
+        res.status(201).json(user)
     },
     update: (req: Request, res: Response) => {
-
+        const {name, email} = req.body
+        const user = userService.update(Number(req.params.id), name, email)
+        user ? res.json(user) : res.status(404).json({message:"Usuario nao encontraddoo"})
     },
     delete: (req: Request, res: Response) => {
-        
+        const success = userService.delete(Number(req.params.id))
+        success ? res.json({message: "usuario removido com sucesoidas"}) : res.status(404).json({message:"Usuario nao encontraddoo"})
     }
 }
